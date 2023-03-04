c72898c01f17f9d52c68f9104745df03a8040e16
riogod <riogod@gmail.com>
2023-03-04 17:37:26 +0300

    fix

--- apps/api/cmd/main.go
+++ apps/api/cmd/main.go
@@ -1,17 +1,5 @@
 package main
 
-import (
-	"fmt"
-
-	model "github.com/riogod/todo/libs/gomodels"
-)
-
 func main() {
-	a := model.ToDoItem{
-		ID:          "a",
-		Title:       "test",
-		Description: "test desc",
-		Status:      "no status",
-	}
-	fmt.Println("test", a)
+
 }
