af6f89dc0f3d2e8f1b72c1f5053c366ded71fded
riogod <riogod@gmail.com>
2023-03-07 13:43:31 +0300

    add some validation

--- apps/api/internal/api/v1/todo_list/controller.go
+++ apps/api/internal/api/v1/todo_list/controller.go
@@ -2,17 +2,12 @@ package todo_list
 
 import (
 	"fmt"
-	"log"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
 	model "github.com/riogod/todo/libs/gomodels"
 	"gorm.io/gorm"
 )
-
-type Success struct {
-	success bool
-}
 
 func Setup(router *gin.RouterGroup, db *gorm.DB) {
 	sevice := &Service{
@@ -33,28 +28,72 @@ func get(s *Service) func(ctx *gin.Context) {
 	return func(ctx *gin.Context) {
 		model, _ := s.GetAll()
 
-		ctx.JSON(http.StatusOK, model)
+		ctx.JSON(http.StatusOK, ResponseOK_DTO{
+			Success: true,
+			Body:    model,
+		})
 	}
 }
 
 func getById(s *Service) func(ctx *gin.Context) {
 	return func(ctx *gin.Context) {
 		id := ctx.Param("id")
-		model, _ := s.GetByID(id)
+		model, err := s.GetByID(id)
+		if err != nil {
 
-		ctx.JSON(http.StatusOK, model)
+			ctx.JSON(http.StatusOK, ResponseERROR_DTO{
+				Success: false,
+				Error: ResponseErrorDTO{
+					ErrorCode:    "NOTHING_FOUND",
+					ErrorMessage: fmt.Sprintf("not found item with id=%v", id),
+				},
+			})
+			return
+		}
+
+		ctx.JSON(http.StatusOK, ResponseOK_DTO{
+			Success: true,
+			Body: ResponseTodoListItemDTO{
+				ID:          fmt.Sprintf("%d", model.ID),
+				Title:       model.Title,
+				Description: model.Description,
+				Status:      model.Status,
+				CreatedAt:   model.CreatedAt,
+				UpdatedAt:   model.UpdatedAt,
+			},
+		})
 	}
 }
 
 func create(s *Service) func(ctx *gin.Context) {
 	return func(ctx *gin.Context) {
 		var createParams model.ToDoItemList
-		err := ctx.BindJSON(&createParams)
+		var requestParams RequestTodoListDTO
+
+		err := ctx.BindJSON(&requestParams)
+
+		createParams.Title = requestParams.Title
+		createParams.Description = requestParams.Description
+		createParams.Status = requestParams.Status
+
 		if err != nil {
-			log.Fatal(err)
+			ctx.JSON(http.StatusOK, ResponseOK_DTO{
+				Success: false,
+			})
 		}
+		createParams.ID = 0
 		s.Create(&createParams)
-		ctx.JSON(http.StatusOK, createParams)
+		ctx.JSON(http.StatusOK, ResponseOK_DTO{
+			Success: true,
+			Body: ResponseTodoListItemDTO{
+				ID:          fmt.Sprintf("%d", createParams.ID),
+				Title:       createParams.Title,
+				Description: createParams.Description,
+				Status:      createParams.Status,
+				CreatedAt:   createParams.CreatedAt,
+				UpdatedAt:   createParams.UpdatedAt,
+			},
+		})
 	}
 
 }
@@ -63,15 +102,40 @@ func update(s *Service) func(ctx *gin.Context) {
 	return func(ctx *gin.Context) {
 		id := ctx.Param("id")
 		var updateParams model.ToDoItemList
-		err := ctx.BindJSON(&updateParams)
+		var requestParams RequestTodoListDTO
+
+		err := ctx.BindJSON(&requestParams)
+
+		updateParams.Title = requestParams.Title
+		updateParams.Description = requestParams.Description
+		updateParams.Status = requestParams.Status
+
 		if err != nil {
 			fmt.Println(err)
 		}
-		model, err := s.Update(id, &updateParams)
+		err = s.Update(id, &updateParams)
 		if err != nil {
-			fmt.Println(err)
+
+			ctx.JSON(http.StatusOK, ResponseERROR_DTO{
+				Success: false,
+				Error: ResponseErrorDTO{
+					ErrorCode:    "NOTHING_TO_UPDATE",
+					ErrorMessage: fmt.Sprintf("not found item with id=%v for update", id),
+				},
+			})
+			return
 		}
-		ctx.JSON(http.StatusOK, model)
+		ctx.JSON(http.StatusOK, ResponseOK_DTO{
+			Success: true,
+			Body: ResponseTodoListItemDTO{
+				ID:          fmt.Sprintf("%d", updateParams.ID),
+				Title:       updateParams.Title,
+				Description: updateParams.Description,
+				Status:      updateParams.Status,
+				CreatedAt:   updateParams.CreatedAt,
+				UpdatedAt:   updateParams.UpdatedAt,
+			},
+		})
 	}
 
 }
@@ -83,8 +147,9 @@ func delete(s *Service) func(ctx *gin.Context) {
 		if err != nil {
 			fmt.Println(err)
 		}
-		ctx.JSON(http.StatusOK, Success{
-			success: true,
+		ctx.JSON(http.StatusOK, ResponseOK_DTO{
+			Success: true,
+			Body:    nil,
 		})
 	}
 
